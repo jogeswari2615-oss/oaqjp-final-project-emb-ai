b4c21a76569931a6d1af11213475f06c2b508bb7
Jogeswari <jogeswari371@gmail.com>
2026-09-22 14:52:24 -0400

    Complete Task 6 Flask application deployment

---
+++ server.py
@@ -0,0 +1,26 @@
+"""Executing this function initiates the application of emotion
+detection to be executed over the Flask channel and deployed on
+localhost:5000.
+"""from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+app = Flask("Emotion Detector")
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    anger = response.get('anger')
+    disgust = response.get('disgust')
+    fear = response.get('fear')
+    joy = response.get('joy')
+    sadness = response.get('sadness')
+    dominant_emotion = response.get('dominant_emotion')
+    return (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and "
+        f"'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    )
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
